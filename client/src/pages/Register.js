fddd7770aa2e746363d71bea9d75d08cd03a7393
duy321111 <duy321111@gmail.com>
2025-09-16 21:25:13 +0700

    implement user register and login with bcrypt & jwt

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const Register = () => {
-  return (
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Mật khẩu nhập lại không khớp!");
+      return;
+    }
+
+    try {
+      const res = await axios.post("http://localhost:5000/auth/register", {
+        name,
+        email,
+        password,
+      });
+      alert(res.data.message);
+      navigate("/login"); // Chuyển sang trang login sau khi đăng ký
+    } catch (err) {
+      if (err.response) {
+
+        alert(err.response.data.message);
+      } else if (err.request) {
+        
+        alert("Không nhận được phản hồi từ server");
+      } else {
+
+        alert(err.message);
+      }
+    }
+
+  };
+
+  return ( 
     <div className="app__container">
       <Header />
         <div class="modal__body">
@@ -19,33 +57,40 @@ const Register = () => {
                         Đăng nhập
                       </Link>
                     </div>
+                    <form className="auth-form__form" onSubmit={handleSubmit}>
+                        <div className="auth-form__form">
+                          <div className="auth-form__group">
 
-                    <div className="auth-form__form">
-                      <div className="auth-form__group">
-                        <input type="text" className="auth-form_input" placeholder="Email của bạn" />
-                      </div>
+                            <input type="text" className="auth-form_input" placeholder="Họ tên" value={name}
+                            onChange={(e) => setName(e.target.value)}/>
+                            
+                            <input 
+                              type="text" className="auth-form_input" placeholder="Email của bạn" value={email}
+                              onChange={(e) => setEmail(e.target.value)}
+                            />
+                        
+                            <input type="password" className="auth-form_input" placeholder="Mật khẩu" value={password}
+                            onChange={(e) => setPassword(e.target.value)}/>
+                       
 
-                      <div className="auth-form__group">
-                        <input type="password" className="auth-form_input" placeholder="Mật khẩu" />
-                      </div>
+                            <input type="password" className="auth-form_input" placeholder="Nhập lại mật khẩu" value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)} />
+                          </div>
+                        </div>
+                        
+                        <div className="auth-form__aside">
+                          <p className="auth-form__policy-text">
+                            Bằng việc đăng ký, bạn đã đồng ý với chúng tôi về
+                            <a href="/" className="auth-form__text-link"> Điều khoản dịch vụ</a> &
+                            <a href="/" className="auth-form__text-link"> Chính sách bảo mật</a>
+                          </p>
+                        </div>
 
-                      <div className="auth-form__group">
-                        <input type="password" className="auth-form_input" placeholder="Nhập lại mật khẩu" />
-                      </div>
-                    </div>
-
-                    <div className="auth-form__aside">
-                      <p className="auth-form__policy-text">
-                        Bằng việc đăng ký, bạn đã đồng ý với chúng tôi về
-                        <a href="/" className="auth-form__text-link"> Điều khoản dịch vụ</a> &
-                        <a href="/" className="auth-form__text-link"> Chính sách bảo mật</a>
-                      </p>
-                    </div>
-
-                    <div className="auth-form__control">
-                      <Link to="/" className="btn btn--normal auth-form__control-back">TRỞ LẠI</Link>
-                      <button className="btn btn--primary">ĐĂNG KÝ</button>
-                    </div>
+                        <div className="auth-form__control">
+                          <Link to="/" className="btn btn--normal auth-form__control-back">TRỞ LẠI</Link>
+                          <button type ="submit" className="btn btn--primary">ĐĂNG KÝ</button>
+                        </div>
+                    </form>
                   </div>
 
                   <div className="auth-form__social">
---
+++ server/controllers/authController.js
@@ -0,0 +1,49 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Kiểm tra user đã tồn tại
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: "Email đã tồn tại" });
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Tạo user mới
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "Đăng ký thành công!" });
+  } catch (err) {
+    console.error("Lỗi register:", err);
+    res.status(500).json({ message: "Server lỗi" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { name, email, password} = req.body;
+    
+    //Tim user
+
+    const user = await User.findOne({email});
+    if(!user) return res.status(400).json({message:"Sai tài khoản hoặc mật khẩu"})
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) return res.status(400).json({message:"Sai tài khoản hoặc mật khẩu"})
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.json({ message: "Đăng nhập thành công", token });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Lỗi server" });
+    }
+}
+
+

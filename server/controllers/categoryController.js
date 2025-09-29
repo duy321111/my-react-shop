671198e8d4bc01ef7beae8895a79665c1b6580bf
duy321111 <duy321111@gmail.com>
2025-09-29 22:11:03 +0700

    Fix: category page image loading issue

---
+++ server/controllers/categoryController.js
@@ -0,0 +1,21 @@
+import Category from "../models/Category.js";
+import Brand from "../models/Brand.js";
+
+// Lấy danh sách brands theo category
+export const getBrandsByCategory = async (req, res) => {
+  try {
+    const { categoryNameOrId } = req.params;
+
+    // Tìm category theo name (ignore case)
+    const category = await Category.findOne({
+    name: new RegExp(`^${categoryNameOrId}$`, "i") // i = ignore case
+    }).populate("brands");
+    ;
+
+    if (!category) return res.status(404).json({ message: "Category not found" });
+
+    res.json(category.brands); // trả về mảng brands
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};

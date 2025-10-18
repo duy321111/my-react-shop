4da10fb3b7a0495f7676c928dd18787a28214c3b
duy321111 <duy321111@gmail.com>
2025-10-18 23:30:59 +0700

    fix: reponsive profile page

--- client/src/components/Header/SearchArea.js
+++ client/src/components/Header/SearchArea.js
@@ -233,7 +233,7 @@ export default function Header() {
               alt="avatar"
               className="header__navbar-user-img"
             />
-            <span className="header__navbar-user-name">{user.name.split(" ").slice(0, 2).join(" ")}</span>
+            
             <ul className="header__navbar-user-menu">
               <li className="header__navbar-user-item">
                 <Link to="/profile">Tài khoản của tôi</Link>

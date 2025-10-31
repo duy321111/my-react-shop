16c4b763c4f872e02734b3d86b308e7437592d20
duy321111 <duy321111@gmail.com>
2025-10-31 21:37:02 +0700

    login reponsive mobile

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -91,16 +91,46 @@ const Login = () => {
                   </div>
                 </div>
 
-                <div className="auth-form__social">
-                  <a href="/" className="auth-form__social--facebook btn btn--size-s btn--with-icon">
-                    <i className="auth-form__social-icon fa-brands fa-square-facebook"></i>
-                    <span className="auth-form__social--title">Kết nối với Facebook</span>
-                  </a>
-                  <a href="/" className="auth-form__social--google btn btn--size-s btn--with-icon">
-                    <i className="auth-form__social-icon fa-brands fa-google"></i>
-                    <span className="auth-form__social--title">Kết nối với Google</span>
-                  </a>
-                </div>
+                  <div className="auth-form__social hide-on-mobile">
+                    <a href="/" className="auth-form__social--facebook btn btn--size-s btn--with-icon">
+                      <img 
+                        src="/img/icons8-facebook-48.png" 
+                        alt="Facebook" 
+                        className="auth-form__social-icon"
+                      />
+                      <span className="auth-form__social--title">Kết nối với Facebook</span>
+                    </a>
+                    <a href="/" className="auth-form__social--google btn btn--size-s btn--with-icon">
+                      <img 
+                        src="/img/icons8-google-48.png" 
+                        alt="Google" 
+                        className="auth-form__social-icon"
+                      />
+                      <span className="auth-form__social--title">Kết nối với Google</span>
+                    </a>
+                  </div>
+
+                  <div className="auth-form__social hide-on-pc-tablet">
+                    <a href="/" className="auth-form__social--facebook btn btn--size-s btn--with-icon">
+                      <img 
+                        src="/img/icons8-facebook-48.png" 
+                        alt="Facebook" 
+                        className="auth-form__social-icon"
+                      />
+                      <span className="auth-form__social--title">Kết nối với Facebook</span>
+                    </a>
+
+                    <a href="/" className="auth-form__social--google btn btn--size-s btn--with-icon">
+                      <img 
+                        src="/img/icons8-google-48.png" 
+                        alt="Google" 
+                        className="auth-form__social-icon"
+                      />
+                      <span className="auth-form__social--title">Kết nối với Google</span>
+                    </a>
+                  </div>
+
+
 
               </div>
             </div>

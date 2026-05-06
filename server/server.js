6571c5e16b1c30047dcfdeb63491615cd6379b79
Daji-36 <nguyenduongdai31@gmail.com>
2026-05-06 20:39:29 +0700

    fix: render env port and cors

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path";
 import authRoutes from "./routes/auth.js";
 import uploadRoutes from "./routes/upload.js";
 import productRoutes from "./routes/product.js";
@@ -20,7 +19,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+  })
+);
 app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
@@ -47,4 +50,5 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error(err));
 
 // Start server
-app.listen(5000, () => console.log("Server running on 5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on ${port}`));
